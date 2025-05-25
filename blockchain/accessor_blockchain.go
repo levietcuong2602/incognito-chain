31368e4fcef858c32620c4bc1d795d3101f0f38f
Justin <justin@vmi1997479.contaboserver.net>
2025-05-25 12:19:54 -0400

    bugfix: update new block beacon throw panic nil pointer

--- blockchain/accessor_blockchain.go
+++ blockchain/accessor_blockchain.go
@@ -17,7 +17,7 @@ import (
 	"github.com/incognitochain/incognito-chain/dataaccessobject/rawdbv2"
 )
 
-//get beacon block hash by height, with current view
+// get beacon block hash by height, with current view
 func (blockchain *BlockChain) GetBeaconBlockHashByHeight(finalView, bestView multiview.View, height uint64) (*common.Hash, error) {
 
 	blkheight := bestView.GetHeight()
@@ -133,7 +133,7 @@ func (blockchain *BlockChain) GetBeaconBlockByHash(beaconBlockHash common.Hash) 
 	return beaconBlock, uint64(len(beaconBlockBytes)), nil
 }
 
-//SHARD
+// SHARD
 func (blockchain *BlockChain) GetShardBlockHashByHeight(finalView, bestView multiview.View, height uint64) (*common.Hash, error) {
 
 	blkheight := bestView.GetHeight()
@@ -274,7 +274,7 @@ func (blockchain *BlockChain) GetShardBlockByHash(hash common.Hash) (*types.Shar
 	return nil, 0, NewBlockChainError(GetShardBlockByHashError, fmt.Errorf("Not found shard block by hash %+v", hash))
 }
 
-//traverse finalview back to certain block height from beacon chain
+// traverse finalview back to certain block height from beacon chain
 func (blockchain *BlockChain) GetShardBlockForBridge(from uint64, to common.Hash, newBeaconBlock *types.BeaconBlock, blockShardStates map[byte][]types.ShardState) (map[byte][]*types.ShardBlock, map[uint64]map[byte][]*types.ShardBlock, error) {
 	beaconBlk, _, _ := blockchain.GetBeaconBlockByHash(to)
 	shardBlksForBridge := map[byte][]*types.ShardBlock{}
@@ -342,6 +342,9 @@ func (blockchain *BlockChain) GetShardBlockForBeaconProducer(bestShardHeights ma
 			if tempShardBlock.GetHeight() <= bestShardHeight {
 				break
 			}
+			if pointerView == nil {
+				break
+			}
 			listShardBlock[pointerView.GetHeight()] = tempShardBlock
 
 			pointerView = blockchain.ShardChain[shardID].multiView.GetViewByHash(tempShardBlock.GetPrevHash())
@@ -353,6 +356,7 @@ func (blockchain *BlockChain) GetShardBlockForBeaconProducer(bestShardHeights ma
 					Logger.log.Errorf("Cannot get block at height %v", shardHeight)
 					return nil
 				}
+				break
 			} else {
 				tempShardBlock = pointerView.GetBlock().(*types.ShardBlock)
 			}
@@ -640,7 +644,7 @@ func (blockchain *BlockChain) GetBeaconRootsHash(height uint64) (*BeaconRootHash
 	return bRH, err
 }
 
-//GetStakerInfo : Return staker info from statedb
+// GetStakerInfo : Return staker info from statedb
 func (beaconBestState *BeaconBestState) GetStakerInfo(stakerPubkey string) (*statedb.StakerInfo, bool, error) {
 	return statedb.GetStakerInfo(beaconBestState.consensusStateDB.Copy(), stakerPubkey)
 }
